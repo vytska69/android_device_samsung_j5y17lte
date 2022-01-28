3407c1a08d1fd35dee9e6513ce922fc7ddd50ceb
vytska69 <vytautas.duchas@gmail.com>
2022-01-28 20:59:25 +0200

    Update lineage_j5y17lte.mk

--- lineage_j5y17lte.mk
+++ lineage_j5y17lte.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/j5y17lte/device.mk)
 
 # Inherit some common Aosp stuff.
-$(call inherit-product, vendor/lineage/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5y17lte
